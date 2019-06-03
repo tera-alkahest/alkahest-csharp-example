c503a657867969050677661a3af7c60d6c730518
Alex Rønne Petersen <alex@alexrp.com>
2019-06-03 02:14:58 +0200

    Update to new API.

--- Main.cs
+++ Main.cs
@@ -2,44 +2,39 @@ using Alkahest.Core;
 using Alkahest.Core.Logging;
 using Alkahest.Core.Net.Game;
 using Alkahest.Core.Net.Game.Packets;
+using Alkahest.Plugins.CSharp;
 using System.Linq;
 
 namespace Alkahest.Scripts.Example
 {
     public static class ExampleScript
     {
-        static Log _log;
+        static CSharpScriptContext _context;
 
         static bool HandleCheckVersion(GameClient client, Direction direction, CCheckVersionPacket packet)
         {
             foreach (var ver in packet.Versions)
-                _log.Info("Client reported version: {0}", ver.Value);
+                _context.Log.Info("Client reported version: {0}", ver.Value);
 
             return true;
         }
 
-        // The special function __Start__ is invoked on startup. The proxies
-        // parameter is an array of Alkahest.Core.Net.Game.GameProxy objects.
-        // The log parameter is an Alkahest.Core.Logging.Log object created
-        // specifically for this script package.
-        public static void __Start__(GameProxy[] proxies, Log log)
+        public static void __Start__(CSharpScriptContext context, GameProxy[] proxies)
         {
-            _log = log;
+            _context = context;
 
             foreach (var proc in proxies.Select(x => x.Processor))
                 proc.AddHandler<CCheckVersionPacket>(HandleCheckVersion);
 
-            _log.Basic("Started example script");
+            _context.Log.Basic("Started example script");
         }
 
-        // The special function __Stop__ is invoked on shutdown and receives the
-        // same parameters that __Start__ did.
-        public static void __Stop__(GameProxy[] proxies, Log log)
+        public static void __Stop__(CSharpScriptContext context, GameProxy[] proxies)
         {
             foreach (var proc in proxies.Select(x => x.Processor))
                 proc.RemoveHandler<CCheckVersionPacket>(HandleCheckVersion);
 
-            _log.Basic("Stopped example script");
+            _context.Log.Basic("Stopped example script");
         }
     }
 }
