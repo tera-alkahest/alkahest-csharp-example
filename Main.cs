28a1c4be103b7688a11eeee70eed8769441d3e61
Alex Rønne Petersen <alex@alexrp.com>
2019-07-04 05:27:18 +0200

    Update to new packet handler API.

--- Main.cs
+++ Main.cs
@@ -1,9 +1,5 @@
-using Alkahest.Core;
-using Alkahest.Core.Logging;
-using Alkahest.Core.Net.Game;
 using Alkahest.Core.Net.Game.Packets;
 using Alkahest.Plugins.CSharp;
-using System.Linq;
 
 namespace Alkahest.Scripts.Example
 {
@@ -11,29 +7,23 @@ namespace Alkahest.Scripts.Example
     {
         static CSharpScriptContext _context;
 
-        static bool HandleCheckVersion(GameClient client, Direction direction, CCheckVersionPacket packet)
-        {
-            foreach (var ver in packet.Versions)
-                _context.Log.Info("Client reported version: {0}", ver.Value);
-
-            return true;
-        }
-
         public static void __Start__(CSharpScriptContext context)
         {
             _context = context;
 
-            foreach (var proc in context.Proxies.Select(x => x.Processor))
-                proc.AddHandler<CCheckVersionPacket>(HandleCheckVersion);
+            context.Dispatch.AddHandler<CCheckVersionPacket>((client, direction, packet, flags) =>
+            {
+                foreach (var ver in packet.Versions)
+                    _context.Log.Info("Client reported version: {0}", ver.Value);
+
+                return true;
+            });
 
             context.Log.Basic("Started example script");
         }
 
         public static void __Stop__(CSharpScriptContext context)
         {
-            foreach (var proc in context.Proxies.Select(x => x.Processor))
-                proc.RemoveHandler<CCheckVersionPacket>(HandleCheckVersion);
-
             context.Log.Basic("Stopped example script");
         }
     }
